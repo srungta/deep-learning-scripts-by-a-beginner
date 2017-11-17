55c92fb400cfdc0add7a91674e3960b8bc9fd03b
Shaswat Rungta <shaswatrungta@hotmail.com>
2017-11-17 23:50:04 +0530

    Added image helpers

--- [C] Convolutional Neural Networks/ConvolutionalNeuralNetworks/ConvolutionalNeuralNetworks.py
+++ [C] Convolutional Neural Networks/ConvolutionalNeuralNetworks/ConvolutionalNeuralNetworks.py
@@ -1,1 +1,10 @@
-﻿
+﻿from HandyScripts import ImageHelper
+
+#Read contents of the image
+filename = "images/img1.jpg"
+image = ImageHelper.get_image(filename)
+
+#Convert to Black and white stream
+bnwimage = ImageHelper.convert_to_gray(image)
+
+ImageHelper.show_image(bnwimage, 'gray')
---
+++ [C] Convolutional Neural Networks/ConvolutionalNeuralNetworks/HandyScripts/ImageHelper.py
@@ -0,0 +1,13 @@
+import matplotlib.image as mpimg
+import matplotlib.pyplot as plt
+import numpy as np
+
+def convert_to_gray(rgb):
+    return np.dot(rgb[...,:3],[0.299,0.587,0.114])
+
+def show_image(bnwimage, stream):
+    plt.imshow(bnwimage, cmap = plt.get_cmap(stream))
+    plt.show()
+
+def get_image(filename):
+    return mpimg.imread(filename)
