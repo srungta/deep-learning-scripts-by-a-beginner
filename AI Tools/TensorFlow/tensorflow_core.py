03db78a61096c1484604f609e6411763c80788b4
Shaswat Rungta <shaswatrungta@hotmail.com>
2017-11-24 19:19:14 +0530

    adding tf core

---
+++ AI Tools/TensorFlow/tensorflow_core.py
@@ -0,0 +1,35 @@
+import tensorflow as tf
+
+# Create variables for weight and bias
+W = tf.Variable([.3], dtype=tf.float32)
+b = tf.Variable([-.3], dtype=tf.float32)
+x = tf.placeholder(dtype=tf.float32)
+linear_model = W * x + b
+y = tf.placeholder(tf.float32)
+
+#Model
+squared_deltas  = tf.square(linear_model - y)
+loss = tf.reduce_sum(squared_deltas)
+
+optimiser = tf.train.GradientDescentOptimizer(0.1)
+train = optimiser.minimize(loss)
+
+# training data
+x_train = [1, 2, 3, 4]
+y_train = [0, -1, -2, -3]
+
+#Initialize global variables
+init = tf.global_variables_initializer()
+sess = tf.Session()
+sess.run(init)
+print(sess.run(W))
+
+#Train
+for i in range(1000):
+  sess.run(train, {x: x_train, y: y_train})
+  print(sess.run(W))
+# evaluate training accuracy
+curr_W, curr_b, curr_loss = sess.run([W, b, loss], {x: x_train, y: y_train})
+print("W: %s b: %s loss: %s"%(curr_W, curr_b, curr_loss))
+
+sess.close()
