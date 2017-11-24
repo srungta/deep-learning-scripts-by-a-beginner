efb58fbc0944f15ab135f8679484107d9c0ed46d
Shaswat Rungta <shaswatrungta@hotmail.com>
2017-11-24 20:01:50 +0530

    Adding custom estimator

--- AI Tools/TensorFlow/estimator.py
+++ AI Tools/TensorFlow/estimator.py
@@ -2,14 +2,37 @@
 import numpy as np
 import tensorflow as tf
 
-# NumPy is often used to load, manipulate and preprocess data.
+
+
+#USING CUSTOM ESTIMATOR
+#Declare the fetaure set.  We only have one
+def model_fn(features, labels, mode):
+    #GEt variables
+    W = tf.get_variable(name= "W",shape=[1],dtype=tf.float64)
+    b = tf.get_variable(name= "b",shape=[1],dtype=tf.float64)
+    y = W * features["x"] + b
+
+    #Loss sub graph
+    loss = tf.reduce_sum(tf.square(y - labels))
+    #Training sub graph
+    global_step = tf.train.get_global_step()
+    optimiser = tf.train.GradientDescentOptimizer(0.01)
+    train  = tf.group(optimiser.minimize(loss), tf.assign_add(global_step,1))
+
+    #EstimatorSpec binds the above optimiser to appropriate functionality
+    return tf.estimator.EstimatorSpec(mode= mode, predictions = y, loss = loss, train_op = train)
+
+
+# Declare list of features.  We only have one numeric feature.  There are many
+# other types of columns that are more complicated and useful.
 feature_columns = [tf.feature_column.numeric_column("x", shape=[1])]
 
 # An estimator is the front end to invoke training (fitting) and evaluation
-# (inference). There are many predefined types like linear regression,
+# (inference).  There are many predefined types like linear regression,
 # linear classification, and many neural network classifiers and regressors.
 # The following code provides an estimator that does linear regression.
-estimator = tf.estimator.LinearClassifier(feature_columns)
+#estimator = tf.estimator.LinearRegressor(feature_columns=feature_columns)
+estimator = tf.estimator.Estimator(model_fn = model_fn)
 
 # TensorFlow provides many helper methods to read and set up data sets.
 # Here we use two data sets: one for training and one for evaluation
@@ -19,24 +42,17 @@ x_train = np.array([1., 2., 3., 4.])
 y_train = np.array([0., -1., -2., -3.])
 x_eval = np.array([2., 5., 8., 1.])
 y_eval = np.array([-1.01, -4.1, -7, 0.])
+input_fn = tf.estimator.inputs.numpy_input_fn({"x": x_train}, y_train, batch_size=4, num_epochs=None, shuffle=True)
+train_input_fn = tf.estimator.inputs.numpy_input_fn({"x": x_train}, y_train, batch_size=4, num_epochs=1000, shuffle=False)
+eval_input_fn = tf.estimator.inputs.numpy_input_fn({"x": x_eval}, y_eval, batch_size=4, num_epochs=1000, shuffle=False)
 
-input_fn = tf.estimator.inputs.numpy_input_fn(
-    {"x":x_train},y_train,batch_size=4, num_epochs=None, shuffle=True)
-train_input_fn = tf.estimator.inputs.numpy_input_fn(
-    {"x":x_train},y_train,batch_size=4,num_epochs = 1000, shuffle=False)
-eval_input_fn = tf.estimator.inputs.numpy_input_fn(
-    {"x":x_eval},y_eval,batch_size=4, num_epochs=1000, shuffle=False)
-
-# We can invoke 1000 training steps by invoking the  method and passing the
+# We can invoke 1000 training steps by invoking the method and passing the
 # training data set.
-estimator.train(input_fn, steps=1000)
+estimator.train(input_fn=input_fn, steps=1000)
 
 # Here we evaluate how well our model did.
-train_metrics = estimator.evaluate(train_input_fn)
-eval_metrics = estimator.evaluate(eval_input_fn)
-print("train metrics %s"% train_metrics)
-print("Eval metrics %s"% eval_metrics)
-
-
-
+train_metrics = estimator.evaluate(input_fn=train_input_fn)
+eval_metrics = estimator.evaluate(input_fn=eval_input_fn)
+print("train metrics: %r" % train_metrics)
+print("eval metrics: %r" % eval_metrics)
 
