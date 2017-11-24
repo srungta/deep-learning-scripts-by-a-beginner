cadd01b9e3b052d3aab0cd32a0e4fdd21d792635
Shaswat Rungta <shaswatrungta@hotmail.com>
2017-11-24 20:18:48 +0530

    Custome estimator works

--- AI Tools/TensorFlow/estimator.py
+++ AI Tools/TensorFlow/estimator.py
@@ -1,26 +1,26 @@
-# NumPy is often used to load, manipulate and preprocess data.
 import numpy as np
 import tensorflow as tf
 
-
-
-#USING CUSTOM ESTIMATOR
-#Declare the fetaure set.  We only have one
+# Declare list of features, we only have one real-valued feature
 def model_fn(features, labels, mode):
-    #GEt variables
-    W = tf.get_variable(name= "W",shape=[1],dtype=tf.float64)
-    b = tf.get_variable(name= "b",shape=[1],dtype=tf.float64)
-    y = W * features["x"] + b
-
-    #Loss sub graph
-    loss = tf.reduce_sum(tf.square(y - labels))
-    #Training sub graph
-    global_step = tf.train.get_global_step()
-    optimiser = tf.train.GradientDescentOptimizer(0.01)
-    train  = tf.group(optimiser.minimize(loss), tf.assign_add(global_step,1))
-
-    #EstimatorSpec binds the above optimiser to appropriate functionality
-    return tf.estimator.EstimatorSpec(mode= mode, predictions = y, loss = loss, train_op = train)
+      # Build a linear model and predict values
+      W = tf.get_variable("W", [1], dtype=tf.float64)
+      b = tf.get_variable("b", [1], dtype=tf.float64)
+      y = W*features['x'] + b
+      # Loss sub-graph
+      loss = tf.reduce_sum(tf.square(y - labels))
+      # Training sub-graph
+      global_step = tf.train.get_global_step()
+      optimizer = tf.train.GradientDescentOptimizer(0.01)
+      train = tf.group(optimizer.minimize(loss),
+                       tf.assign_add(global_step, 1))
+      # EstimatorSpec connects subgraphs we built to the
+      # appropriate functionality.
+      return tf.estimator.EstimatorSpec(
+          mode=mode,
+          predictions=y,
+          loss=loss,
+          train_op=train)
 
 
 # Declare list of features.  We only have one numeric feature.  There are many
@@ -38,13 +38,16 @@ estimator = tf.estimator.Estimator(model_fn = model_fn)
 # Here we use two data sets: one for training and one for evaluation
 # We have to tell the function how many batches
 # of data (num_epochs) we want and how big each batch should be.
+
+# define our data sets
 x_train = np.array([1., 2., 3., 4.])
 y_train = np.array([0., -1., -2., -3.])
 x_eval = np.array([2., 5., 8., 1.])
-y_eval = np.array([-1.01, -4.1, -7, 0.])
+y_eval = np.array([-1.01, -4.1, -7., 0.])
 input_fn = tf.estimator.inputs.numpy_input_fn({"x": x_train}, y_train, batch_size=4, num_epochs=None, shuffle=True)
 train_input_fn = tf.estimator.inputs.numpy_input_fn({"x": x_train}, y_train, batch_size=4, num_epochs=1000, shuffle=False)
 eval_input_fn = tf.estimator.inputs.numpy_input_fn({"x": x_eval}, y_eval, batch_size=4, num_epochs=1000, shuffle=False)
+
 
 # We can invoke 1000 training steps by invoking the method and passing the
 # training data set.
@@ -55,4 +58,3 @@ train_metrics = estimator.evaluate(input_fn=train_input_fn)
 eval_metrics = estimator.evaluate(input_fn=eval_input_fn)
 print("train metrics: %r" % train_metrics)
 print("eval metrics: %r" % eval_metrics)
-
