521a39a18d918a12fcc20aa38844034a24e78369
Shaswat Rungta <shaswatrungta@hotmail.com>
2017-11-24 19:39:53 +0530

    Added estimator

--- AI Tools/TensorFlow/linear_regression.py
+++ AI Tools/TensorFlow/linear_regression.py
@@ -7,11 +7,14 @@ x = tf.placeholder(dtype=tf.float32)
 linear_model = W * x + b
 y = tf.placeholder(tf.float32)
 
+#Hyperparameters
+learning_rate =0.01
+
 #Model
 squared_deltas  = tf.square(linear_model - y)
 loss = tf.reduce_sum(squared_deltas)
 
-optimiser = tf.train.GradientDescentOptimizer(0.1)
+optimiser = tf.train.GradientDescentOptimizer(learning_rate)
 train = optimiser.minimize(loss)
 
 # training data
@@ -27,7 +30,7 @@ print(sess.run(W))
 #Train
 for i in range(1000):
   sess.run(train, {x: x_train, y: y_train})
-  print(sess.run(W))
+
 # evaluate training accuracy
 curr_W, curr_b, curr_loss = sess.run([W, b, loss], {x: x_train, y: y_train})
 print("W: %s b: %s loss: %s"%(curr_W, curr_b, curr_loss))
